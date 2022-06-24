1b0be3cf5451635ef09d3d80a806d8b54615b9ad
hlowell <91516214+sergeyeshakin@users.noreply.github.com>
2022-06-24 10:27:01 +0700

    Delete EngineBench directory

--- EngineBench/IEngine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class IEngine {
- public:
-    void virtual setAmbientTemperature(const double) = 0;
-    double virtual getTemperature() = 0;
-    double virtual getOverheatTemperature() = 0;
-    void virtual workForOneSecond() = 0;
-};
